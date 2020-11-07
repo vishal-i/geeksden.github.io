6fed9d7bcd5b80089b0a02e4d418555bbe4e5c7f
Vishal Ingle <vishalingle123@gmail.com>
2020-11-07 16:00:23 +0530

    up

--- dovalidation.js
+++ dovalidation.js
@@ -1,3 +1,9 @@
+console.log("ll");
+
+
+
+
+
 function ValidateEmail(inputText)
 {
 	var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -42,12 +48,33 @@ var mailformat = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 function doFooterValidation()
 {
 	var inputText=document.getElementById("semail");
-	var inputMob=document.getElementById("sphone");
+	var inputMob=document.getElementById("smob");
 	var check=ValidateEmail(inputText);
 	var checksmob=ValidateMob(inputMob);
 	if(check && checksmob)
 	{
-           
+		var playersRef = firebase.database().ref("B");
+		playersRef.once('value', function(snapshot) {
+    				playersRef.child(document.getElementById("smob").value).set({
+     				 
+    				  email:document.getElementById("semail").value,
+     				 mob:document.getElementById("smob").value
+     				 
+  				 }).then(function() {
+    					console.log("We will reach you soon!");
+					   // alert("We will reach you soon!");
+					   document.getElementById("semail").value=null;
+					   document.getElementById("smob").value=null;
+					   document.getElementById("falert").innerHTML="Sent! We will reach you soon! "
+ 				 })
+ 				 .catch(function(error) {
+    					console.error("Error writing document: ", error);
+  				});;
+  
+  				
+		});
+
+		
 	}
 	else
 	{
@@ -58,10 +85,9 @@ function doFooterValidation()
 		}
 		if(!checksmob)
 		{
-			document.getElementById("sphone").value="";
-			document.getElementById("sphone").placeholder="Invalid ! Enter again";
+			document.getElementById("smob").value="";
+			document.getElementById("smob").placeholder="Invalid ! Enter again";
 		}
-
 
 	}
 
@@ -76,7 +102,34 @@ function doValidation()
 	var checkName=ValidateName(inputName);
 	if(checkMob && checkMail && checkName)
 	{
-		
+		 var playersRef = firebase.database().ref("A");
+ 		 var mob=document.getElementById("mob").value;
+		 var milisec=Date.now();
+		 var newkey=mob+"-"+milisec;
+  		 playersRef.once('value', function(snapshot) {
+    				playersRef.child(newkey).set({
+     				 name: document.getElementById("nam").value,
+    				  email:document.getElementById("mail").value,
+     				 mob:document.getElementById("mob").value,
+      				sub:document.getElementById("sub").value,
+      				msg:document.getElementById("msg").value
+     				 
+  				 }).then(function() {
+    					console.log("We will reach you soon!");
+					//	alert("We will reach you soon!");
+						document.getElementById("nam").value=null;
+						document.getElementById("mail").value=null;
+						document.getElementById("mob").value=null;
+						document.getElementById("sub").value=null;
+						document.getElementById("msg").value=null;
+						document.getElementById("formalert").innerHTML="Thank You for contacting us! We'll be in touch soon.";
+ 				 })
+ 				 .catch(function(error) {
+    					console.error("Error writing document: ", error);
+  				});;
+  
+  				
+		});
 	}
 	else
 	{
